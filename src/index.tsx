8d53df84e810941bf4ca124b77392ae8f7007fec
yujhenchen <yujhen.chen93@gmail.com>
2024-06-25 12:08:38 +0800

    feat: add app basic files

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+const container = document.getElementById("app");
+if (container) {
+  const root = createRoot(container);
+  root.render(<App />);
+} else {
+  console.error("Cannot find app element");
+}
